f6c4160a81f468dab42a990bfd91765351187ad0
叶质彬 <lip8658@live.com>
2018-03-09 15:48:01 +0800

    sss

---
+++ Kasim.Framework/Kasim.Framework.ZjyxcgWinForm/FrmInvoice.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Kasim.Framework.ZjyxcgWinForm
+{
+    partial class FrmInvoice
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.c1InputPanel1 = new C1.Win.C1InputPanel.C1InputPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.c1InputPanel1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // c1InputPanel1
+            // 
+            this.c1InputPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.c1InputPanel1.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F);
+            this.c1InputPanel1.Location = new System.Drawing.Point(0, 0);
+            this.c1InputPanel1.Name = "c1InputPanel1";
+            this.c1InputPanel1.Size = new System.Drawing.Size(846, 150);
+            this.c1InputPanel1.TabIndex = 0;
+            // 
+            // FrmInvoice
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(846, 552);
+            this.Controls.Add(this.c1InputPanel1);
+            this.Name = "FrmInvoice";
+            this.Text = "C018票据上传";
+            ((System.ComponentModel.ISupportInitialize)(this.c1InputPanel1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private C1.Win.C1InputPanel.C1InputPanel c1InputPanel1;
+    }
+}
