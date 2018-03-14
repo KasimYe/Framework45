d066dbe31809cc60067d2c2b2f488f3f0b254cb6
叶质彬 <lip8658@live.com>
2018-03-14 09:17:00 +0800

    dss

--- Kasim.Framework/Kasim.Framework.ZjyxcgWinForm/FrmInvoice.Designer.cs
+++ Kasim.Framework/Kasim.Framework.ZjyxcgWinForm/FrmInvoice.Designer.cs
@@ -29,7 +29,11 @@
         private void InitializeComponent()
         {
             this.c1InputPanel1 = new C1.Win.C1InputPanel.C1InputPanel();
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
             ((System.ComponentModel.ISupportInitialize)(this.c1InputPanel1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // c1InputPanel1
@@ -41,15 +45,34 @@
             this.c1InputPanel1.Size = new System.Drawing.Size(846, 150);
             this.c1InputPanel1.TabIndex = 0;
             // 
+            // gridControl1
+            // 
+            this.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridControl1.Location = new System.Drawing.Point(0, 150);
+            this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.Size = new System.Drawing.Size(846, 402);
+            this.gridControl1.TabIndex = 1;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            // 
+            // gridView1
+            // 
+            this.gridView1.GridControl = this.gridControl1;
+            this.gridView1.Name = "gridView1";
+            // 
             // FrmInvoice
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(846, 552);
+            this.Controls.Add(this.gridControl1);
             this.Controls.Add(this.c1InputPanel1);
             this.Name = "FrmInvoice";
             this.Text = "C018票据上传";
             ((System.ComponentModel.ISupportInitialize)(this.c1InputPanel1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -57,5 +80,7 @@
         #endregion
 
         private C1.Win.C1InputPanel.C1InputPanel c1InputPanel1;
+        private DevExpress.XtraGrid.GridControl gridControl1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
     }
 }
